59f7b6567d3d1437f57c30b49ef3f27c6afddbac
dm-dmytrenko <dmytro.dmytrenko.prog@gmail.com>
2024-05-06 22:15:53 +0200

    add header component with styles

---
+++ src/components/header/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './header.scss';
+
+const Header: React.FC = () => {
+  return (
+    <div className='header-container'>
+      <header className="gradient-text">
+        Calling All Athletes Age 8 - 29!
+      </header>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/landing-page/landing-page.tsx
+++ src/pages/landing-page/landing-page.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../../components/header/header';
 
 const LandingPage: React.FC = () => {
   return (
     <div>
+      <Header />
       Landing page
     </div>
   );
