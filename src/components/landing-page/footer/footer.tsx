88f4d0f8a533fb2bd36df9beeb949608083fce7d
dm-dmytrenko <dmytro.dmytrenko.prog@gmail.com>
2024-05-08 22:41:59 +0200

    fix footer in main page

--- src/components/landing-page/footer/footer.tsx
+++ src/components/landing-page/footer/footer.tsx
@@ -7,11 +7,12 @@ const Footer: React.FC = () => {
   return (
     <div className='footer-container'>
       <div className='footer-top'>
-        <p>Disclaimer:  Participation is subject to passing the health assessment and interview process.  All information provided will be used solely for the purpose of this program. </p>
+        <p>Disclaimer: Participation is subject to passing the health assessment and interview process.  All information provided will be used solely for the purpose of this program. </p>
       </div>
       <GradientBox height='39px' />
       <div className='footer-botom'>
-        <p>SOSX Inc. 2022 Terms and conditions apply. <a href='#'>Cookies Policy</a>.</p>
+        <p
+        >SOSX Inc. 2022 Terms and conditions apply. <a href='#'>Cookies Policy</a>.</p>
       </div>
     </div>
   );
---
+++ src/features/scrollToTop/scrollToTop.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+// const ScrollToTop: React.FC = ({ children }) => {
+//   const location = useLocation();
+
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, [location]);
+
+//   return <>{children}</>;
+// };
+
+// export default ScrollToTop;
